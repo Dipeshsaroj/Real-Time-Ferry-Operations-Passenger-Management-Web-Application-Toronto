72df0f9e36563a0917dadce4ff84b1a7bfdbf495
Dipeshsaroj <177411699+Dipeshsaroj@users.noreply.github.com>
2025-06-26 05:48:22 +0000

    datatype

---
+++ basics/08_comp.js
@@ -0,0 +1,8 @@
+console. log (null > 0); 
+console. log (null == 0); //compariosn operator me null as a 0 treat hota hai equal to me nahi
+console. log (null >= 0);
+console. log (undefined == 0); 
+console. log(undefined <0);
+console. log(undefined >0)
+console. log ("2" ==2);//not check data types 
+console. log ("2" === 12);//check datatypes
---
+++ basics/09dataty.js
@@ -0,0 +1,18 @@
+//Primitive
+ // 7 types : String, Number, Boolearn, null, undefined, Symbol,
+//BigInt
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+//non primittive datatype is function
+const heros = ["shaktiman", "naagraj", "doga"]; //datatype-object
+let myObj = {
+name: "hitesh", age: 22,
+}
+const myFunction = function (){
+     console. log("hello world");//datatype-function(object function)
+}
+console.log(typeof(heros));
+// https://262.ecma-international.org/5.1/#sec-11.4.3. for read datatypes
