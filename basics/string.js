c426b989131364f3367b30e2ba4734b4562fb2e6
Dipeshsaroj <177411699+Dipeshsaroj@users.noreply.github.com>
2025-06-27 17:57:33 +0000

    date

---
+++ basics/string.js
@@ -0,0 +1,31 @@
+const name = "Dipesh"
+
+const repoCount = 50
+
+
+
+// console. log (name + repoCount + " Value");
+// console. log (`Hello my name is ${name} and my repo count is ${repoCount}`);
+// const gameName = new String ('dipeshsaroj')
+// // console. log (gameName [1]);
+// // console. log (gameName.__proto__);
+// // console. log (gameName.length);
+// // console. log (gameName.toUpperCase());
+// // console. log (gameName.charAt (2)); 
+// // console. log (gameName.indexOf('t'));
+
+// console. log (gameName.charAt(2));
+// console. log (gameName.indexOf('s'));
+// const newstring = gameName.substring(0, 4)//start is 0 ,4 is length
+// console.log(newstring);
+// const anotherString = gameName.slice(-8, 4) // start index -8 from last start -1(length-8==3) start index is 3,stop before index 4
+// console. log(anotherString);
+const game = new String ('dip-esh-saroj')
+const newStringOne = " dipesh "
+console.log (newStringOne); 
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console. log(url. replace('%20', '-'))
+console. log(url. includes ('sundar'))
+console. log (game.split('-'));
